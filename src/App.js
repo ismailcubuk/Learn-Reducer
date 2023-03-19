992934ee4ac75245615b4207f85970003c062bc3
ismailcubuk <iismailcubuk@gmail.com>
2023-03-19 20:15:29 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer, useState } from "react";
 
+const INCREMENT_COUNT = 'increment';
+const DECREMENT_COUNT = 'decrement'
+const SET_VALUE_TO_ADD = 'change_value_to_add'
+const ADD_VALUE_TO_COUNT = 'add_value_to_count'
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case INCREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count + 1
+      };
+    case DECREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count - 1
+      };
+    case ADD_VALUE_TO_COUNT:
+      return {
+        ...state,
+        count: state.count + state.valueToAdd,
+        valueToAdd: 0
+      }
+    case SET_VALUE_TO_ADD:
+      return {
+        ...state,
+        valueToAdd: action.payload,
+      };
+    default:
+      throw new Error('unexpected action type: ' + action.type);
+    // return state;
+  }
+}
 function App() {
+  const [state, dispatch] = useReducer(reducer, {
+    count: 0,
+    valueToAdd: 0
+  });
+
+  console.log(state);
+
+
+  const increment = () => {
+    dispatch({
+      type: INCREMENT_COUNT,
+    });
+  }
+  const decrement = () => {
+    dispatch({
+      type: DECREMENT_COUNT,
+    })
+  }
+  const handleChange = (e) => {
+    const value = parseInt(e.target.value) || 0;
+    dispatch({
+      type: SET_VALUE_TO_ADD,
+      payload: value
+    })
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    dispatch({
+      type: ADD_VALUE_TO_COUNT,
+    })
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>Count is  {state.count} </h1>
+      <div>
+        <button onClick={increment} >increment</button>
+        <button onClick={decrement} >decrement</button>
+      </div>
+      <br />
+      <form onSubmit={handleSubmit}>
+        <label>Add a lot!</label>
+        <input value={state.valueToAdd || ""} onChange={handleChange} type="number" />
+        <button>Add it !</button>
+      </form>
     </div>
   );
 }
